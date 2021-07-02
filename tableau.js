d8e5cb29aa8ca757f0b5fe04f536a104dc433f25
pabnguyen <phananh.nguyen.vn@gmail.com>
2021-07-02 14:58:06 -0400

    put in javascript file

---
+++ tableau.js
@@ -0,0 +1,14 @@
+function initializeViz() {
+  // JS object that points at empty div in the html
+  var placeholderDiv = document.getElementById("tableauViz");
+  // URL of the viz to be embedded
+  var url = "https://public.tableau.com/views/VietnamCollegeEntranceExamScore2020/Dashboard1?:embed=true&:showVizHome=no";
+  // An object that contains options specifying how to embed the viz
+  var options = {
+    width: '600px',
+    height: '600px',
+    hideTabs: true,
+    hideToolbar: true,
+  };
+  viz = new tableau.Viz(placeholderDiv, url, options);
+}
